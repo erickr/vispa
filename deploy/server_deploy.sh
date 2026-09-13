ea95194f6da3c8e72a1a1adafc4ca3f47b4ac1e3
Eric Krona <eric@krona.tm>
2026-09-13 22:15:15 +0200

    Prepared for vispa deploy

---
+++ deploy/server_deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+(php artisan down ) || true
+    # Update codebase
+    git fetch origin deploy
+    git reset --hard origin/deploy
+
+    # Install dependencies based on lock file
+    composer install --no-interaction --prefer-dist --optimize-autoloader
+
+    # Migrate database
+    php artisan migrate --force
+
+    # Clear cache
+    php artisan optimize
+
+    php artisan config:clear
+
+    # Reload PHP to update opcache
+    #echo "" | sudo -S service php7.4-fpm reload
+# Exit maintenance mode
+php artisan up
+
+echo "Application deployed!"
